59f5ed90b5f5dfcf30929aee4d18b8a847d4179e
unknown <nataliabravojimenez@gmail.com>
2022-02-20 21:08:24 -0500

    practicing react with css/installing fontawesome

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import { fah } from "@fortawesome/free-regular-svg-icons";
+import { faHome} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './App.css';
+import hulu from './hulu.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <div className='flex'>
+      <img src={hulu} alt ="hulu" className='hul'/>
+      < FontAwesomeIcon icon={faHome} className= 'home' style={{cursor:"pointer"}}/>
+      </div>
       </header>
+      <div className='body'>
+      <p>
+          soy el cuerpo
+        </p>
+      </div>
     </div>
   );
 }
