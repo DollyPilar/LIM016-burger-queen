37d2fd46c8e85a5b94278e1e7d8ce75a4030ec0f
DollyPilar <llyatidum@gmail.com>
2022-02-21 20:29:29 -0500

    modularización y reglas eslint

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 // // import logo from './logo.svg';
-import { faSignOut, faHome, faShop} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faSignOut, faHome, faShop } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './App.css';
-import logoDos from './hp_ccexpress.png';
-import oh from './oh.png'
+import logoDos from './img/logo.png';
+import oh from './img/oh.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
-      <div className='flex'>
-        <img src={logoDos} alt ="hulu" className='hul'/>
-        <img src={oh} alt ="hulu" className='order'/>
-          <FontAwesomeIcon icon ={faSignOut} className= 'signOut' />
-          <FontAwesomeIcon icon ={faShop} className= 'home' />
-        < FontAwesomeIcon icon={faHome} className= 'home' style={{cursor:"pointer"}}/>
+        <div className="flex">
+          <img src={logoDos} alt="hulu" className="hul" />
+          <img src={oh} alt="hulu" className="order" />
+          {/* <FontAwesomeIcon icon={faSignOut} className="signOut" />
+          <FontAwesomeIcon icon={faShop} className="home" />
+          <FontAwesomeIcon icon={faHome} className="home" style={{ cursor: 'pointer' }} /> */}
         </div>
       </header>
-      <div className='body'>
-      <p>
+      <div className="body">
+        <p>
           soy el cuerpo
         </p>
       </div>
