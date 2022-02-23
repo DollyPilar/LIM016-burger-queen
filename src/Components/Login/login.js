fa4d91c5f9bcb3b6acc9383ec57249ced3b03a58
DollyPilar <llyatidum@gmail.com>
2022-02-22 20:33:11 -0500

    avance maquetacción login

--- src/Components/Login/login.js
+++ src/Components/Login/login.js
@@ -3,17 +3,23 @@ import { Link } from 'react-router-dom';
 import './login.css';
 import logo from '../../img/logo.png';
 function Log() {
-    return (
+   return (
       <div className="Log">
          <div className = 'Logo'>
           <img src={logo} alt="logo" className="logo"/>
          </div>
+         <div className='form'>
+<label>
+   <h2> ¡BIENVENIDOS A HAPPY PAWS!</h2>
+    <input type="text" placeholder='Correo' name="name" /> <br/>
+    <input type="text" placeholder='Contraseña' name="name" />
+    <p>¿Olvidaste tu contraseña?</p>
+    <button>INICIAR SESIÓN</button>
+    <p>¿No tienes una cuenta?</p>
+    <p>Regístrate</p>
+  </label>
+  </div>
          
-         <div className='Buttons'>
-            <button className='styleBtn'><Link to="/cliente" className='link'>CLIENTE</Link></button>
-            <button className='styleBtn'><Link to="/" className='link'>PERSONAL</Link></button>
-
-         </div>
         
          
         </div>
