e340a20ebc3e223176b9bf7fc5bb4f399c79b60e
unknown <nataliabravojimenez@gmail.com>
2022-03-14 21:42:54 -0500

    se trae la info de las compras con onSnapshot

--- src/Components/Store/StoreProducts.jsx
+++ src/Components/Store/StoreProducts.jsx
@@ -1,31 +1,15 @@
 import React from "react";
 import { IndividualStoreProducts } from "./IndividualStoreProducts.jsx";
 
-export const StoreProducts = ({ compras }) => {
-  // if (compras.length > 0) {
-  //   console.log(
-  //     compras.map((e) => {
-  //       const mmn = e.finalProducts.productsInformation;
-  //       return mmn.map((ee) => ee.Nombre);
-  //     })
-  //   );
-  // }
-  //console.log(compras);
-  // if (compras.length > 0) {
-  //   compras.map((compra) =>
-  //   console.log(compra.ID) )
-  // }
+export const StoreProducts = ({ compras, updateState }) => {
   return (
     <React.Fragment>
-      {/* {compras.length > 0 &&
-        compras.map((compra) => (
-          <IndividualStoreProducts key={compra.ID} compra={compra} />
-        ))} */}
       {compras.length > 0 &&
-        compras.map((compra, index) => (
+        compras.map((compra) => (
           <IndividualStoreProducts
             key={compra.ID}
             compra={compra}
+            updateState={updateState}
           />
         ))}
     </React.Fragment>
--- src/Components/Store/store.jsx
+++ src/Components/Store/store.jsx
@@ -1,53 +1,67 @@
 import React, { useState, useEffect } from "react";
 import { db } from "../../firebase/firebase-config.jsx";
-import { getDocs, where, query, orderBy,  collection } from "firebase/firestore";
+import {
+  onSnapshot,
+  where,
+  query,
+  orderBy,
+  collection,
+  updateDoc,
+  doc,
+} from "firebase/firestore";
 import { NavBar } from "../HomePage/NavBar/NavBar.jsx";
 import { StoreProducts } from "./StoreProducts.jsx";
+import "./store.css";
 
 function Store() {
   const [compras, setCompras] = useState("");
-  // const [general, setGeneral] = useState("");
-
-  const getShoppingProducts = async () => {
+  const getShoppingColle = () => {
     const collRef = collection(db, "compras");
-    const order = query(collRef, where("finalProducts.shoppingState", "==", "Pedido realizado"), orderBy("finalProducts.dateOfShopping", "desc"))
-    try {
-      const allColl = await getDocs(order);
+    const order = query(
+      collRef,
+      where("finalProducts.shoppingState", "==", "Pedido realizado"),
+      orderBy("finalProducts.dateOfShopping", "desc")
+    );
+    onSnapshot(order, (querySnapshot) => {
       const shoppArray = [];
-
-      allColl.forEach((doc) => {
+      querySnapshot.forEach((doc) => {
         let data = doc.data();
         data.ID = doc.id;
-
         shoppArray.push(data);
       });
       setCompras(shoppArray);
+    });
+  };
+  useEffect(() => {
+    getShoppingColle();
+  }, []);
+  const updateState = async (compra) => {
+    console.log("click");
+    // console.log(compra.finalProducts.shoppingState);
+    // console.log(compra.finalProducts.dateOfShopping);
+    const prodRef = doc(db, "compras", compra.ID);
+    try {
+      await updateDoc(prodRef, {
+        // finalProducts: {
+        //   shoppingState:
+        //   dateOfShopping: ,
+        // },
+        "finalProducts.shoppingState": "Pedido Listo",
+        "finalProducts.dateOfShopping": Date.now(),
+      });
     } catch (e) {
       console.log(e);
     }
   };
-  useEffect(() => {
-    getShoppingProducts();
-  }, []);
-
-  // console.log(Array.isArray(compras));
-
-  // compras.productos.map((producto)=>{
-  //   console.log(producto.Nombre)
-  // })
-
-  // const timeOfShopping = compras.hora;
-  // const date = new Date(timeOfShopping);
-  // const myDate = `
-  //         ${date.getHours()}:
-  //         ${date.getMinutes()}`;
 
   return (
     <React.Fragment>
       <NavBar />
-      <StoreProducts compras={compras} />
-
-      <div>Soy la vista del almacén</div>
+      <div className="boxContainer">
+        <div className="storeBox">
+          <StoreProducts compras={compras} updateState={updateState} />
+        </div>
+      </div>
     </React.Fragment>
   );
 }
