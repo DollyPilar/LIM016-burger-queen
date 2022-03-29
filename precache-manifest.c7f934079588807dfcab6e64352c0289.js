8e7f4f27447f7b69195892a27f7c1ec83227889b
DollyPilar <llyatidum@gmail.com>
2022-03-29 12:23:17 -0500

    Updates

---
+++ precache-manifest.c7f934079588807dfcab6e64352c0289.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6f1403d619df21ac3fe5a534e9cd0c2",
+    "url": "/DollyPilar/LIM016-burger-queen/tree/principal/index.html"
+  },
+  {
+    "revision": "b409e81bf8375f7bcb36",
+    "url": "/DollyPilar/LIM016-burger-queen/tree/principal/static/css/main.e4ac648f.chunk.css"
+  },
+  {
+    "revision": "9337fca0bff9c346259d",
+    "url": "/DollyPilar/LIM016-burger-queen/tree/principal/static/js/2.d2d6f12a.chunk.js"
+  },
+  {
+    "revision": "67cca913334021a9d56e9bd2636f009a",
+    "url": "/DollyPilar/LIM016-burger-queen/tree/principal/static/js/2.d2d6f12a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f2df6572341ae076609",
+    "url": "/DollyPilar/LIM016-burger-queen/tree/principal/static/js/3.64351a00.chunk.js"
+  },
+  {
+    "revision": "b409e81bf8375f7bcb36",
+    "url": "/DollyPilar/LIM016-burger-queen/tree/principal/static/js/main.6f8c42d0.chunk.js"
+  },
+  {
+    "revision": "3f4b36d97192780f4507",
+    "url": "/DollyPilar/LIM016-burger-queen/tree/principal/static/js/runtime-main.73f6b96f.js"
+  },
+  {
+    "revision": "8ed7e6b3a5d09c5c7410f0dc47d7213d",
+    "url": "/DollyPilar/LIM016-burger-queen/tree/principal/static/media/PawLogo.8ed7e6b3.png"
+  },
+  {
+    "revision": "8802072f091b1e6e6c80e806cff557ce",
+    "url": "/DollyPilar/LIM016-burger-queen/tree/principal/static/media/admin.8802072f.png"
+  },
+  {
+    "revision": "72ab590953f7f2d15f628c46dda62d84",
+    "url": "/DollyPilar/LIM016-burger-queen/tree/principal/static/media/dogHome.72ab5909.png"
+  },
+  {
+    "revision": "aeab69838dbea412864a7481ee0f1419",
+    "url": "/DollyPilar/LIM016-burger-queen/tree/principal/static/media/peoplewithdogjpg.aeab6983.jpg"
+  }
+]);
